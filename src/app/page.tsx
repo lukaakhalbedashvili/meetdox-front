91b577a4f8f6c7bd63cb723ff48af89de131e3a7
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2023-03-17 01:51:44 +0400

    fix ( eslint errors fix )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
+const Home = () => {
   return <main className="bg-white h-[500px]">asijnk</main>
 }
+
+export default Home
--- src/elements/NavigationBarItems/index.tsx
+++ src/elements/NavigationBarItems/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { FC } from 'react'
+import React, { FC } from 'react'
 interface NavigationBarItemProps {
   href: string
   children: React.ReactNode

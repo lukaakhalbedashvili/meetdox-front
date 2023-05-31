1535f0143b0fe64d05775b498720031f8a23d2f7
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2023-06-01 01:18:51 +0400

    fix ( ts enums as keys )

--- src/components/DashboardTeacherMeetsContent /index.tsx
+++ src/components/DashboardTeacherMeetsContent /index.tsx
@@ -10,10 +10,6 @@ import {
 } from '@/utils/services/time'
 import { scheduleSteps } from '@/data/scheduleSteps'
 import useDashboardTeacherMeetsContent from './useDashboardTeacherMeetsContent'
-import {
-  ScheduleComponentStructureNames,
-  ScheduleTypes,
-} from '../Dashboard/dashboard.interface'
 
 const DashboardTeacherMeetsContent = () => {
   const { completedMeets, currentMeets, mutate, refetch } =
@@ -28,198 +24,151 @@ const DashboardTeacherMeetsContent = () => {
           </h1>
 
           <div className="lg: mb-8 rounded-md py-4 sm:bg-background_gray lg:px-8">
-            {meetings.map((meeting) => (
-              <div
-                key={meeting.teacherImg}
-                className="meeting-card mt-4 mb-4 rounded-md bg-sky bg-opacity-20 py-4 px-4 lg:px-8"
-              >
-                <div className="flex flex-col sm:flex-row sm:space-x-4">
-                  <div className="mb-2 flex w-full justify-center overflow-hidden rounded-full sm:h-16 sm:w-16">
-                    <Image
-                      src={
-                        meeting.teacherImg
-                          ? meeting.clientImg
-                          : '/unknown_user.png'
-                      }
-                      alt="Google Logo"
-                      width={64}
-                      height={64}
-                    />
-                  </div>
-
-                  <div className="flex flex-col">
-                    <div className="flex items-center justify-center space-x-2 sm:justify-start">
-                      <h2 className="font-medium">{meeting.clientUsername}</h2>
-                    </div>
-
-                    <div className="mt-1 flex items-center space-x-2">
-                      <CalendarIcon className="h-4 w-4 text-icon_gray" />
-
-                      <span className="text-sm text-icon_gray">
-                        {formatScheduledDate(meeting.date, meeting.timeZone)}
-                      </span>
-                    </div>
-
-                    <div className="mt-1 flex items-center space-x-2">
-                      <TimeIcon className="h-4 w-4 text-icon_gray" />
-
-                      <span className="text-sm text-icon_gray">
-                        {convertToLocalTime(meeting.time, meeting.timeZone)}
-                        {/* {getTimeAgo(meeting.createdAt)} */}
-                      </span>
-                    </div>
-                    <div className="mt-1 flex items-center space-x-2">
-                      <BiHistory className="h-4 w-4 text-icon_gray" />
-
-                      <span className="text-sm text-icon_gray">
-                        Scheduled {getTimeAgo(meeting.createdAt)}
-                      </span>
-                    </div>
-                    <div className="mt-3 flex w-full items-center justify-center space-x-1 sm:justify-start">
-                      <span className="w-1/3 rounded-xl bg-green_label bg-opacity-20 px-3 py-2 text-xs font-medium text-green_label">
-                        {meeting.duration} min
-                      </span>
-
-                      <span className="w-1/3 rounded-xl bg-blue_label bg-opacity-20 px-3 py-2 text-xs font-medium text-blue_label">
-                        ${(meeting.perHour / 60) * meeting.duration}
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="mb-2 mt-2 flex-col rounded-xl bg-green_label bg-opacity-20 p-3 sm:mt-0 sm:mb-0 sm:ml-2 sm:flex sm:w-1/3">
-                    <p className="text-xs font-medium text-green_label">
-                      {meeting.comment}
-                    </p>
-                  </div>
-
-                  <div className="flex flex-col justify-between sm:ml-2 sm:w-1/3">
-                    <span
-                      className={`
-                    ${
-                      scheduleSteps[ScheduleTypes.MEETINGS_AS_TEACHER][
-                        meeting.status
-                      ][ScheduleComponentStructureNames.TITLE1] === ''
-                        ? 'hidden'
-                        : 'block'
-                    }
-                    
-                    mb-2 w-full rounded-xl bg-blue_label bg-opacity-20 px-3 py-2 text-center text-sm font-medium text-blue_label`}
-                    >
-                      {
-                        scheduleSteps[ScheduleTypes.MEETINGS_AS_TEACHER][
-                          meeting.status
-                        ][ScheduleComponentStructureNames.TITLE1]
-                      }
-                    </span>
-
-                    <span
-                      className={`
-                    ${
-                      scheduleSteps[ScheduleTypes.MEETINGS_AS_TEACHER][
-                        meeting.status
-                      ][ScheduleComponentStructureNames.TITLE2] === ''
-                        ? 'hidden'
-                        : 'block'
-                    } mb-2 w-full rounded-xl bg-orange_label bg-opacity-20 px-3 py-2 text-center text-sm font-medium text-orange_label`}
-                    >
-                      {
-                        scheduleSteps[ScheduleTypes.MEETINGS_AS_TEACHER][
-                          meeting.status
-                        ][ScheduleComponentStructureNames.TITLE2]
-                      }
-                    </span>
-
-                    <div className="flex w-full justify-center sm:space-x-2">
-                      <Button
-                        onClickHandler={() =>
-                          mutate(
-                            {
-                              newStatus:
-                                scheduleSteps[
-                                  ScheduleTypes.MEETINGS_AS_TEACHER
-                                ][meeting.status][
-                                  ScheduleComponentStructureNames
-                                    .ON_BUTTON_RED_CLICK
-                                ],
-                              meetId: meeting.meetId,
-                              teacherUid: meeting.teacherUid,
-                              clientUid: meeting.clientUid,
-                            },
-                            {
-                              onSuccess: () => {
-                                refetch()
-                              },
-                              onError: () => {},
-                            }
-                          )
+            {meetings.map((meeting) => {
+              const currMeet = scheduleSteps.meetingsAsTeacher[meeting.status]
+              return (
+                <div
+                  key={meeting.teacherImg}
+                  className="meeting-card mt-4 mb-4 rounded-md bg-sky bg-opacity-20 py-4 px-4 lg:px-8"
+                >
+                  <div className="flex flex-col sm:flex-row sm:space-x-4">
+                    <div className="mb-2 flex w-full justify-center overflow-hidden rounded-full sm:h-16 sm:w-16">
+                      <Image
+                        src={
+                          meeting.teacherImg
+                            ? meeting.clientImg
+                            : '/unknown_user.png'
                         }
-                        customTailwindClasses={` ${
-                          scheduleSteps[ScheduleTypes.MEETINGS_AS_TEACHER][
-                            meeting.status
-                          ][ScheduleComponentStructureNames.BUTTON_RED] === ''
-                            ? 'hidden'
-                            : 'block'
-                        } bg-error bg-opacity-20 border-border_gray w-1/2 `}
-                      >
-                        <div className="flex h-[35px] items-center justify-center">
-                          <p
-                            className={`flex items-center justify-center text-sm font-medium text-error`}
-                          >
-                            {
-                              scheduleSteps[ScheduleTypes.MEETINGS_AS_TEACHER][
-                                meeting.status
-                              ][ScheduleComponentStructureNames.BUTTON_RED]
-                            }
-                          </p>
-                        </div>
-                      </Button>
+                        alt="Google Logo"
+                        width={64}
+                        height={64}
+                      />
+                    </div>
 
-                      <Button
-                        onClickHandler={() =>
-                          mutate(
-                            {
-                              newStatus:
-                                scheduleSteps[
-                                  ScheduleTypes.MEETINGS_AS_TEACHER
-                                ][meeting.status][
-                                  ScheduleComponentStructureNames
-                                    .ON_BUTTON_GREEN_CLICK
-                                ],
-                              meetId: meeting.meetId,
-                              teacherUid: meeting.teacherUid,
-                              clientUid: meeting.clientUid,
-                            },
-                            {
-                              onSuccess: () => {},
-                              onError: () => {},
+                    <div className="flex flex-col">
+                      <div className="flex items-center justify-center space-x-2 sm:justify-start">
+                        <h2 className="font-medium">
+                          {meeting.clientUsername}
+                        </h2>
+                      </div>
+
+                      <div className="mt-1 flex items-center space-x-2">
+                        <CalendarIcon className="h-4 w-4 text-icon_gray" />
+
+                        <span className="text-sm text-icon_gray">
+                          {formatScheduledDate(meeting.date, meeting.timeZone)}
+                        </span>
+                      </div>
+
+                      <div className="mt-1 flex items-center space-x-2">
+                        <TimeIcon className="h-4 w-4 text-icon_gray" />
+
+                        <span className="text-sm text-icon_gray">
+                          {convertToLocalTime(meeting.time, meeting.timeZone)}
+                        </span>
+                      </div>
+                      <div className="mt-1 flex items-center space-x-2">
+                        <BiHistory className="h-4 w-4 text-icon_gray" />
+
+                        <span className="text-sm text-icon_gray">
+                          Scheduled {getTimeAgo(meeting.createdAt)}
+                        </span>
+                      </div>
+
+                      <div className="mt-3 flex w-full items-center justify-center space-x-1 sm:justify-start">
+                        <span className="w-1/3 rounded-xl bg-green_label bg-opacity-20 px-3 py-2 text-xs font-medium text-green_label">
+                          {meeting.duration} min
+                        </span>
+
+                        <span className="w-1/3 rounded-xl bg-blue_label bg-opacity-20 px-3 py-2 text-xs font-medium text-blue_label">
+                          ${(meeting.perHour / 60) * meeting.duration}
+                        </span>
+                      </div>
+                    </div>
+
+                    <div className="mb-2 mt-2 flex-col rounded-xl bg-green_label bg-opacity-20 p-3 sm:mt-0 sm:mb-0 sm:ml-2 sm:flex sm:w-1/3">
+                      <p className="text-xs font-medium text-green_label">
+                        {meeting.comment}
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col justify-between sm:ml-2 sm:w-1/3">
+                      {currMeet.title1 && (
+                        <span className="mb-2 w-full rounded-xl bg-blue_label bg-opacity-20 px-3 py-2 text-center text-sm font-medium text-blue_label">
+                          {currMeet.title1}
+                        </span>
+                      )}
+
+                      {currMeet.title2 && (
+                        <span className="mb-2 w-full rounded-xl bg-orange_label bg-opacity-20 px-3 py-2 text-center text-sm font-medium text-orange_label">
+                          {currMeet.title2}
+                        </span>
+                      )}
+
+                      <div className="flex w-full justify-center sm:space-x-2">
+                        {currMeet.buttonRed && (
+                          <Button
+                            onClickHandler={() =>
+                              mutate(
+                                {
+                                  newStatus: currMeet.onButtonRedClick,
+                                  meetId: meeting.meetId,
+                                  teacherUid: meeting.teacherUid,
+                                  clientUid: meeting.clientUid,
+                                },
+                                {
+                                  onSuccess: () => {
+                                    refetch()
+                                  },
+                                  onError: () => {},
+                                }
+                              )
                             }
-                          )
-                        }
-                        customTailwindClasses={` ${
-                          scheduleSteps[ScheduleTypes.MEETINGS_AS_TEACHER][
-                            meeting.status
-                          ][ScheduleComponentStructureNames.BUTTON_GREEN] === ''
-                            ? 'hidden'
-                            : 'block'
-                        } bg-success_border_green bg-opacity-20 border-border_gray w-1/2`}
-                      >
-                        <div className="flex h-[35px]  items-center justify-center">
-                          <p
-                            className={`flex items-center justify-center text-sm font-medium text-success_border_green`}
+                            customTailwindClasses="bg-error bg-opacity-20 border-border_gray w-1/2"
                           >
-                            {
-                              scheduleSteps[ScheduleTypes.MEETINGS_AS_TEACHER][
-                                meeting.status
-                              ][ScheduleComponentStructureNames.BUTTON_GREEN]
+                            <div className="flex h-[35px] items-center justify-center">
+                              <p
+                                className={`flex items-center justify-center text-sm font-medium text-error`}
+                              >
+                                {currMeet.buttonRed}
+                              </p>
+                            </div>
+                          </Button>
+                        )}
+
+                        {currMeet.buttonGreen && (
+                          <Button
+                            onClickHandler={() =>
+                              mutate(
+                                {
+                                  newStatus: currMeet.onButtonGreenClick,
+                                  meetId: meeting.meetId,
+                                  teacherUid: meeting.teacherUid,
+                                  clientUid: meeting.clientUid,
+                                },
+                                {
+                                  onSuccess: () => {},
+                                  onError: () => {},
+                                }
+                              )
                             }
-                          </p>
-                        </div>
-                      </Button>
+                            customTailwindClasses="bg-success_border_green bg-opacity-20 border-border_gray w-1/2`"
+                          >
+                            <div className="flex h-[35px]  items-center justify-center">
+                              <p
+                                className={`flex items-center justify-center text-sm font-medium text-success_border_green`}
+                              >
+                                {currMeet.buttonGreen}
+                              </p>
+                            </div>
+                          </Button>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              )
+            })}
           </div>
         </>
       ))}
--- src/data/scheduleSteps.ts
+++ src/data/scheduleSteps.ts
@@ -1,130 +1,107 @@
 import {
   ScheduleSteps,
   ScheduleStepStatus,
-  ScheduleTypes,
 } from '@/components/Dashboard/dashboard.interface'
-import { ScheduleComponentStructureNames } from '../components/Dashboard/dashboard.interface'
-/* eslint-disable unused-imports/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-unused-vars */
 
 export const scheduleSteps: ScheduleSteps = {
-  [ScheduleTypes.MEETINGS_AS_CLIENT]: {
-    [ScheduleStepStatus.SCHEDULED_BY_USER]: {
-      [ScheduleComponentStructureNames.TITLE1]:
-        'You have scheduled this meeting',
-      [ScheduleComponentStructureNames.TITLE2]:
-        'Waiting teacher for confirmation...',
-      [ScheduleComponentStructureNames.BUTTON_RED]: 'Cancel',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]:
-        ScheduleStepStatus.CANCELED_BY_USER,
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+  meetingsAsClient: {
+    scheduledByUser: {
+      title1: 'You have scheduled this meeting',
+      title2: 'Waiting teacher for confirmation...',
+      buttonRed: 'Cancel',
+      buttonGreen: '',
+      onButtonRedClick: ScheduleStepStatus.CANCELED_BY_USER,
+      onButtonGreenClick: '',
     },
-    [ScheduleStepStatus.CANCELED_BY_USER]: {
-      [ScheduleComponentStructureNames.TITLE1]:
-        'You have canceled this meeting',
-      [ScheduleComponentStructureNames.TITLE2]: '',
-      [ScheduleComponentStructureNames.BUTTON_RED]: '',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+    canceledByUser: {
+      title1: 'You have canceled this meeting',
+      title2: '',
+      buttonRed: '',
+      buttonGreen: '',
+      onButtonRedClick: '',
+      onButtonGreenClick: '',
     },
-    [ScheduleStepStatus.CANCELED_BY_TEACHER]: {
-      [ScheduleComponentStructureNames.TITLE1]:
-        'Teacher has canceled this meeting',
-      [ScheduleComponentStructureNames.TITLE2]: '',
-      [ScheduleComponentStructureNames.BUTTON_RED]: '',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+    canceledByTeacher: {
+      title1: 'Teacher has canceled this meeting',
+      title2: '',
+      buttonRed: '',
+      buttonGreen: '',
+      onButtonRedClick: '',
+      onButtonGreenClick: '',
     },
-    [ScheduleStepStatus.CONFIRMED_BY_TEACHER]: {
-      [ScheduleComponentStructureNames.TITLE1]:
-        'Teacher has confirmed this meeting',
-      [ScheduleComponentStructureNames.TITLE2]: 'Waiting for your payment...',
-      [ScheduleComponentStructureNames.BUTTON_RED]: 'Cancel',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: 'Pay',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]:
-        ScheduleStepStatus.CANCELED_BY_USER,
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]:
-        ScheduleStepStatus.PAID_BY_USER,
+    confirmedByTeacher: {
+      title1: 'Teacher has confirmed this meeting',
+      title2: 'Waiting for your payment...',
+      buttonRed: 'Cancel',
+      buttonGreen: 'Pay',
+      onButtonRedClick: ScheduleStepStatus.CANCELED_BY_USER,
+      onButtonGreenClick: ScheduleStepStatus.PAID_BY_USER,
     },
-    [ScheduleStepStatus.PAID_BY_USER]: {
-      [ScheduleComponentStructureNames.TITLE1]: 'Scheduled Successfully',
-      [ScheduleComponentStructureNames.TITLE2]:
-        'Wait date and time of meeting...',
-      [ScheduleComponentStructureNames.BUTTON_RED]: '',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: 'Join',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+    paidByUser: {
+      title1: 'Scheduled Successfully',
+      title2: 'Wait date and time of meeting...',
+      buttonRed: '',
+      buttonGreen: 'Join',
+      onButtonRedClick: '',
+      onButtonGreenClick: '',
     },
-    [ScheduleStepStatus.COMPLETED]: {
-      [ScheduleComponentStructureNames.TITLE1]: 'Meeting completed',
-      [ScheduleComponentStructureNames.TITLE2]: '',
-      [ScheduleComponentStructureNames.BUTTON_RED]: '',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+    completed: {
+      title1: 'Meeting completed',
+      title2: '',
+      buttonRed: '',
+      buttonGreen: '',
+      onButtonRedClick: '',
+      onButtonGreenClick: '',
     },
   },
-  [ScheduleTypes.MEETINGS_AS_TEACHER]: {
-    [ScheduleStepStatus.SCHEDULED_BY_USER]: {
-      [ScheduleComponentStructureNames.TITLE1]:
-        'You have scheduled this meeting',
-      [ScheduleComponentStructureNames.TITLE2]:
-        'Waiting for your confirmation...',
-      [ScheduleComponentStructureNames.BUTTON_RED]: 'Cancel',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: 'Confirm',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]:
-        ScheduleStepStatus.CANCELED_BY_TEACHER,
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]:
-        ScheduleStepStatus.CONFIRMED_BY_TEACHER,
+  meetingsAsTeacher: {
+    scheduledByUser: {
+      title1: 'User have scheduled this meeting',
+      title2: 'Waiting for your confirmation...',
+      buttonRed: 'Cancel',
+      buttonGreen: 'Confirm',
+      onButtonRedClick: ScheduleStepStatus.CANCELED_BY_TEACHER,
+      onButtonGreenClick: ScheduleStepStatus.CONFIRMED_BY_TEACHER,
     },
-    [ScheduleStepStatus.CANCELED_BY_USER]: {
-      [ScheduleComponentStructureNames.TITLE1]:
-        'User has canceled this meeting',
-      [ScheduleComponentStructureNames.TITLE2]: '',
-      [ScheduleComponentStructureNames.BUTTON_RED]: '',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+    canceledByUser: {
+      title1: 'User has canceled this meeting',
+      title2: '',
+      buttonRed: '',
+      buttonGreen: '',
+      onButtonRedClick: '',
+      onButtonGreenClick: '',
     },
-    [ScheduleStepStatus.CANCELED_BY_TEACHER]: {
-      [ScheduleComponentStructureNames.TITLE1]:
-        'You have canceled this meeting',
-      [ScheduleComponentStructureNames.TITLE2]: '',
-      [ScheduleComponentStructureNames.BUTTON_RED]: '',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+    canceledByTeacher: {
+      title1: 'You have canceled this meeting',
+      title2: '',
+      buttonRed: '',
+      buttonGreen: '',
+      onButtonRedClick: '',
+      onButtonGreenClick: '',
     },
-    [ScheduleStepStatus.CONFIRMED_BY_TEACHER]: {
-      [ScheduleComponentStructureNames.TITLE1]:
-        'You have confirmed this meeting',
-      [ScheduleComponentStructureNames.TITLE2]: 'Waiting for user payment...',
-      [ScheduleComponentStructureNames.BUTTON_RED]: 'Cancel',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]:
-        ScheduleStepStatus.CANCELED_BY_TEACHER,
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+    confirmedByTeacher: {
+      title1: 'You have confirmed this meeting',
+      title2: 'Waiting for user payment...',
+      buttonRed: 'Cancel',
+      buttonGreen: '',
+      onButtonRedClick: ScheduleStepStatus.CANCELED_BY_TEACHER,
+      onButtonGreenClick: '',
     },
-    [ScheduleStepStatus.PAID_BY_USER]: {
-      [ScheduleComponentStructureNames.TITLE1]: 'Scheduled Successfully',
-      [ScheduleComponentStructureNames.TITLE2]:
-        'Wait date and time of meeting...',
-      [ScheduleComponentStructureNames.BUTTON_RED]: '',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: 'Join',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+    paidByUser: {
+      title1: 'Scheduled Successfully',
+      title2: 'Wait date and time of meeting...',
+      buttonRed: '',
+      buttonGreen: 'Join',
+      onButtonRedClick: '',
+      onButtonGreenClick: '',
     },
-    [ScheduleStepStatus.COMPLETED]: {
-      [ScheduleComponentStructureNames.TITLE1]: 'Meeting completed',
-      [ScheduleComponentStructureNames.TITLE2]: '',
-      [ScheduleComponentStructureNames.BUTTON_RED]: '',
-      [ScheduleComponentStructureNames.BUTTON_GREEN]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_RED_CLICK]: '',
-      [ScheduleComponentStructureNames.ON_BUTTON_GREEN_CLICK]: '',
+    completed: {
+      title1: 'Meeting completed',
+      title2: '',
+      buttonRed: '',
+      buttonGreen: '',
+      onButtonRedClick: '',
+      onButtonGreenClick: '',
     },
   },
 }

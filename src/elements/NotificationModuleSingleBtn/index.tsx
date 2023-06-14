5c645a4b08bb342b4558c581179b20e1f2ae368a
lashamarkhvaidze <lasha.marxvaidze@gmail.com>
2023-06-14 13:28:40 +0400

    feat: on notif click make it read

--- src/elements/NotificationModuleSingleBtn/index.tsx
+++ src/elements/NotificationModuleSingleBtn/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
-import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { getTimeAgo } from '@/utils/services/time'
+import { useMakeNotificationRead } from '@/reactQuery/useMakeNotificationRead'
 
 interface NotificationModuleSingleBtnProps {
   url: string
@@ -21,24 +22,36 @@ const NotificationModuleSingleBtn: FC<NotificationModuleSingleBtnProps> = ({
   title,
   type,
 }) => {
+  const { mutate } = useMakeNotificationRead()
+  const router = useRouter()
   return (
-    <Link href={url} key={id}>
-      <div
-        className={`${type} relative flex flex-col border-b-[1px] border-solid border-border_gray hover:bg-white ${
-          isRead ? 'bg-white' : 'bg-gray'
-        } py-2  px-4 `}
-      >
-        <h3 className="text-sm font-medium">{title}</h3>
-        <div className="flex items-center justify-between">
-          <p className="mb-5 w-80 text-sm text-text_gray line-clamp-3">
-            {text}
-          </p>
-        </div>
-        <div className="absolute bottom-1 flex items-center justify-between">
-          <p className="mt-[2px] text-xs text-lite">{getTimeAgo(msTime)}</p>
-        </div>
+    <div
+      onClick={() => {
+        router.push(url)
+        mutate(
+          {
+            notificationId: id,
+          },
+          {
+            onSuccess: () => {},
+            onError: () => {},
+          }
+        )
+      }}
+      key={id}
+      className={`${type} relative
+      flex cursor-pointer flex-col border-b-[1px] border-solid border-border_gray hover:bg-white ${
+        isRead ? 'bg-white' : 'bg-gray'
+      } py-2  px-4 `}
+    >
+      <h3 className="text-sm font-medium">{title}</h3>
+      <div className="flex items-center justify-between">
+        <p className="mb-5 w-80 text-sm text-text_gray line-clamp-3">{text}</p>
       </div>
-    </Link>
+      <div className="absolute bottom-1 flex items-center justify-between">
+        <p className="mt-[2px] text-xs text-lite">{getTimeAgo(msTime)}</p>
+      </div>
+    </div>
   )
 }
 

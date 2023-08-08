2328772f8344b0fe09a1e46744c27aa486e9886a
Lasha Markhvaidze <lasha.marxvaidze@gmail.com>
2023-08-08 21:24:30 +0400

    feat/backend prod

--- src/components/LandingBaner/index.tsx
+++ src/components/LandingBaner/index.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 const LandingBaner = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center">
-      <h1 className="text-4xl font-medium text-sky">Schedule your meet</h1>
-      <p className="mt-2 text-base font-thin text-text_gray">
+      <h1 className="px-4 text-center text-3xl font-medium text-sky">
+        Schedule your meet
+      </h1>
+      <p className="mt-2 px-4 text-center text-base font-thin text-text_gray">
         Find your perfect teacher today and start learning
       </p>
       <div className="mt-8 flex w-full flex-col items-center justify-center sm:flex-row sm:items-start sm:justify-start">

b9b31020099e40b6d7bc894a11f5d39d1c2eeb40
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2023-08-23 11:20:27 +0400

    feat ( opengraph description test1 ) m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,28 @@ export const metadata: Metadata = {
     google:
       'google-site-verification=-hgu1-kSQmU-ucbQtDHrTPiz5puuqz3635K5kJjBCbI',
   },
+  openGraph: {
+    title: 'Meetdox',
+    description:
+      "Do you have a question? Let's schedule a meeting with an expert to get your answer.",
+    url: 'https://nextjs.org',
+    siteName: 'Meetdox',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {

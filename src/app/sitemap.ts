ee9f637d49208c1680fdfdda026f40d5b0ef7856
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2023-08-23 13:40:11 +0400

    fix ( incorrect baseurl )

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import { categories } from '@/data/categoryItems'
 import { API_URL } from '@/utils/consts/consts'
 
 const sitemap = async () => {
+  const baseUrl = 'https://www.meetdox.com/'
+
   const expertsRes = await fetch(
     `${API_URL}/users/teacher/get-teachers-uids-for-seo`
   )
@@ -9,26 +11,26 @@ const sitemap = async () => {
   const expertsData = await expertsRes.json()
 
   const expertsUrls = expertsData.data.map((item: string) => {
-    return { url: `${API_URL}/expert/${item}`, lastModified: new Date() }
+    return { url: `${baseUrl}/expert/${item}`, lastModified: new Date() }
   })
 
   const categoryUrls = categories.map((item) => {
-    return { url: `${API_URL}/category/${item.url}`, lastModified: new Date() }
+    return { url: `${baseUrl}/category/${item.url}`, lastModified: new Date() }
   })
 
   const subCategories = categories.map((item) => {
     return item.subCategories.map((subItem) => {
       return {
-        url: `${API_URL}/category/${item.url}/${subItem.url}`,
+        url: `${baseUrl}/category/${item.url}/${subItem.url}`,
         lastModified: new Date(),
       }
     })
   })
 
   return [
-    { url: API_URL, lastModified: new Date() },
-    { url: `${API_URL}/about`, lastModified: new Date() },
-    { url: `${API_URL}/become-expert`, lastModified: new Date() },
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/about`, lastModified: new Date() },
+    { url: `${baseUrl}/become-expert`, lastModified: new Date() },
     ...expertsUrls,
     ...categoryUrls,
     ...subCategories[0],

ee2f9402b28ff5159b7c2e2a08c9bab55b03a79a
Lasha Markhvaidze <lasha.marxvaidze@gmail.com>
2023-08-24 15:56:26 +0400

    fix/faq ui and mobile responsive fixed

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -7,25 +7,25 @@ const Page = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null)
 
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="container mx-auto flex flex-col md:flex-row">
-        <div className="relative h-screen w-1/2 p-8">
+    <div className="flex min-h-full items-start justify-center">
+      <div className="container mx-auto mt-8 flex h-full flex-col md:mt-12 md:flex-row">
+        <div className="relative w-full  md:w-1/2">
           <Image
             src="/meetdox-mockup.jpeg"
             fill
             alt="FAQ Image"
-            className="relative top-0 h-full w-full object-contain"
+            className="relative top-0 h-60 w-full object-contain md:h-auto"
           />
         </div>
-        <div className="p-8 md:w-1/2">
-          <h1 className="mt-18 mb-8 text-3xl font-bold">
+        <div className="w-full p-4 md:w-1/2 md:p-8">
+          <h1 className="mb-4 text-xl font-medium md:mb-8 md:text-2xl">
             Frequently Asked Questions
           </h1>
           <div className="space-y-4">
             {faqData.map((faq, index) => (
               <div
                 key={index}
-                className="rounded-xl border bg-white p-4 shadow-md"
+                className="rounded-xl border bg-white p-3 shadow-md"
               >
                 <div
                   className="flex cursor-pointer justify-between"
@@ -35,10 +35,16 @@ const Page = () => {
                       : setActiveIndex(index)
                   }
                 >
-                  <h3 className="text-lg font-medium">{faq.question}</h3>
+                  <h3 className="text-sm font-medium md:text-base">
+                    {faq.question}
+                  </h3>
                   <span>{activeIndex === index ? '-' : '+'}</span>
                 </div>
-                {activeIndex === index && <p className="mt-2">{faq.answer}</p>}
+                {activeIndex === index && (
+                  <p className="mt-2 text-xs font-thin md:text-sm">
+                    {faq.answer}
+                  </p>
+                )}
               </div>
             ))}
           </div>
